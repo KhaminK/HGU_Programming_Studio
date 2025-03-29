b9acc4a87c61ec88691bf17e282b9130beb3f8da
KhaminK <hamin5y5@gmail.com>
2025-03-30 01:24:21 +0900

    Made a folder for reviewing Java programming methods

---
+++ random/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args) {
+        //Input
+        Scanner s = new Scanner(System.in);
+        //Get one line of input
+        s.nextLine();
+        //Get one intger
+        s.nextInt();
+    }
+}
