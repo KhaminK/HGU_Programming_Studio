4f88d46f391376df2b3ca08dd56349b833e382b7
KhaminK <hamin5y5@gmail.com>
2025-06-12 23:14:13 +0900

    added cheering comment

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(){
-    //the pointer of its struct can be declared even when the struct is not fully defined yet. 
+    //the pointer of its struct can be declared even when the struct is not fully defined yet.
+    //stay strong. I am with you. 
     return 0;
 }
